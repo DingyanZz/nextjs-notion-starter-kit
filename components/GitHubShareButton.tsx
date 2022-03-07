ffd29b6b40f790ff2fdc223c506321e62c206a7c
DingyanZz <91420449+DingyanZz@users.noreply.github.com>
2022-03-07 17:11:49 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
+      href='https://nextjs-notion-starter-kit-rose-seven.vercel.app/'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
